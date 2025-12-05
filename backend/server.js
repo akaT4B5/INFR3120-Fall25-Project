121dbdf08b73e81883b67723ce67703e8cbd27ec
YNFC <ghayas.sher@ontariotechu.net>
2025-12-05 15:17:33 -0500

    asd

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,8 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static frontend files
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'backend/public')));
+app.use('/uploads', express.static(path.join(__dirname, 'backend/public/uploads')));
 
 // MongoDB connection
 const uri = process.env.MONGO_URI; 
